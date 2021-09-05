dd34fd61011eff04c09007072e2aa5a887507865
sriram-r-b <59769010+sriram-r-b@users.noreply.github.com>
2021-09-05 11:39:55 +0530

    Delete communicate.py

--- rasa-chatbot-docker/models/communicate.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = 'http://creativai.herokuapp.com/webhooks/rest/webhook'
-msg=input()
-myobj = {
-"message": msg,
-"sender" : 1,
-}
-x= requests.post(url,json=myobj)
-print(x.text)
